58cc68dd04b1f16ffd49f3b208966f18a75ff82d
Xavi Rigau <xavi@novoda.com>
2019-02-12 12:21:53 +0100

    Split packages to separate modules

---
+++ articles/src/main/java/com/xrigau/nytimesmostpopular/articles/ArticleAdapter.java
@@ -0,0 +1,68 @@
+package com.xrigau.nytimesmostpopular.articles;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.xrigau.nytimesmostpopular.dummy.DummyContent;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+
+    private final List<DummyContent.DummyItem> items;
+    private final NavigationStrategy navigationStrategy;
+
+    ArticleAdapter(List<DummyContent.DummyItem> items, NavigationStrategy navigationStrategy) {
+        this.items = items;
+        this.navigationStrategy = navigationStrategy;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_content, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        holder.idView.setText(items.get(position).id);
+        holder.contentView.setText(items.get(position).content);
+
+        holder.itemView.setTag(items.get(position));
+        holder.itemView.setOnClickListener(onClickListener);
+    }
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
+            navigationStrategy.navigate(item.id);
+        }
+    };
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView idView;
+        final TextView contentView;
+
+        ViewHolder(View view) {
+            super(view);
+            idView = view.findViewById(R.id.id_text);
+            contentView = view.findViewById(R.id.content);
+        }
+    }
+
+    interface NavigationStrategy {
+        void navigate(String item); // TODO: Use the correct type
+    }
+}
---
+++ articles/src/main/java/com/xrigau/nytimesmostpopular/articles/ArticleListActivity.java
@@ -0,0 +1,77 @@
+package com.xrigau.nytimesmostpopular.articles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.xrigau.nytimesmostpopular.details.ItemDetailActivity;
+import com.xrigau.nytimesmostpopular.details.ItemDetailFragment;
+import com.xrigau.nytimesmostpopular.dummy.DummyContent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ArticleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        RecyclerView recyclerView = findViewById(R.id.item_list);
+        recyclerView.setAdapter(new ArticleAdapter(DummyContent.ITEMS, createNavigationStrategy()));
+    }
+
+    private ArticleAdapter.NavigationStrategy createNavigationStrategy() {
+        if (usingTwoPaneLayout()) {
+            return new TwoPaneStrategy(getSupportFragmentManager());
+        } else {
+            return new NewScreenStrategy(this);
+        }
+    }
+
+    private boolean usingTwoPaneLayout() {
+        return getResources().getBoolean(R.bool.is_tablet);
+    }
+
+    private static class TwoPaneStrategy implements ArticleAdapter.NavigationStrategy {
+
+        private final FragmentManager fragmentManager;
+
+        private TwoPaneStrategy(FragmentManager fragmentManager) {
+            this.fragmentManager = fragmentManager;
+        }
+
+        @Override
+        public void navigate(String item) {
+            Bundle arguments = new Bundle();
+            arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item);
+            ItemDetailFragment fragment = new ItemDetailFragment();
+            fragment.setArguments(arguments);
+            fragmentManager.beginTransaction()
+                    .replace(R.id.item_detail_container, fragment)
+                    .commit();
+        }
+    }
+
+    private static class NewScreenStrategy implements ArticleAdapter.NavigationStrategy {
+
+        private final Context context;
+
+        private NewScreenStrategy(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void navigate(String item) {
+            Intent intent = new Intent(context, ItemDetailActivity.class);
+            intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item);
+            context.startActivity(intent);
+        }
+    }
+}
